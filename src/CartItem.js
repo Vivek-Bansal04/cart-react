522d35795ede3b8ebb49ec39586143cb666aa8a4
Vivek <bansalvivek3142@gmail.com>
2021-04-07 13:25:13 +0530

    added increase func quantity

--- src/CartItem.js
+++ src/CartItem.js
@@ -9,7 +9,14 @@ class CartItem extends React.Component {
             qty: 1,
             img: ''
         }
+       // this.increaseQuantity = this.increaseQuantity.bind(this);
+          //bind unless value of this will be lost
     }
+    //arrow function automatically bind this to instance of class so no need of separately writing bind
+    increaseQuantity = () => {
+        console.log('this', this.state);
+    }
+    
     render () {
         //object destructuring we are getting qty by normal js and others by different method
         const {price,title} = this.state;
@@ -24,9 +31,15 @@ class CartItem extends React.Component {
                     <div style={{ color: '#777' }}>Qty:{this.state.qty}</div>
                     <div className="cart-item-actions">
                        {/* Buttons */}
-                       <img alt="increase" className="action-icons" src="https://as2.ftcdn.net/jpg/01/07/62/07/500_F_107620769_UwNVSoXnKS4VNcOKoZjPohlEPn83oE38.jpg" />
-                       <img alt="decrease" className="action-icons" src="https://as1.ftcdn.net/jpg/03/73/49/86/500_F_373498649_nBxauQ0ipBSVrVcMpWWVmTpXu3BLvRyY.jpg" />
-                       <img alt="delete" className="action-icons" src="https://as2.ftcdn.net/jpg/01/90/89/15/500_F_190891550_N7uKp2aHE3mOc20dmtDytj7atgvbhdOu.jpg" />
+                       <img alt="increase" className="action-icons"
+                        src="https://as2.ftcdn.net/jpg/01/07/62/07/500_F_107620769_UwNVSoXnKS4VNcOKoZjPohlEPn83oE38.jpg"
+                        onClick={this.increaseQuantity}
+                      
+                        />
+                       <img alt="decrease" className="action-icons"
+                        src="https://as1.ftcdn.net/jpg/03/73/49/86/500_F_373498649_nBxauQ0ipBSVrVcMpWWVmTpXu3BLvRyY.jpg" />
+                       <img alt="delete" className="action-icons" 
+                       src="https://as2.ftcdn.net/jpg/01/90/89/15/500_F_190891550_N7uKp2aHE3mOc20dmtDytj7atgvbhdOu.jpg" />
                     </div>
                 </div>
             </div>
